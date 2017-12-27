ee14252f9016b30d588f4b37e686d78723e7082b
Madhura Banerjee <pxdas@bn.com>
2017-12-27 11:16:46 -0500

    Ading new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AlertModule} from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
