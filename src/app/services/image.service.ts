b29a1074d37b4a00549f8338ced4a66667e021c2
Madhura Banerjee <pxdas@bn.com>
2017-12-28 11:08:41 -0500

    branch-2 commit

---
+++ src/app/services/image.service.ts
@@ -0,0 +1,90 @@
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class ImageService{
+
+	getImages(){
+
+
+	}
+	
+}
+const IMAGES = [
+	{
+		'id' : '1',
+		'category' : "Cricket",
+		'description' : "Image 1",
+		'imageurl' : 'assets/images/image1.jpeg'
+
+	},
+	{
+		'id' : '2',
+		'category' : "Cricket",
+		'description' : "Image 2",
+		'imageurl' : 'assets/images/image2.jpeg'
+
+	},
+	{
+		'id' : '3',
+		'category' : "Football",
+		'description' : "Image 3",
+		'imageurl' : 'assets/images/image3.jpeg'
+
+	},
+	{
+		'id' : '4',
+		'category' : "Football",
+		'description' : "Image 4",
+		'imageurl' : 'assets/images/image4.jpeg'
+
+	},
+	{
+		'id' : '5',
+		'category' : "Football",
+		'description' : "Image 5",
+		'imageurl' : 'assets/images/image5.jpeg'
+
+	},
+	{
+		'id' : '6',
+		'category' : "Volly",
+		'description' : "Image 6",
+		'imageurl' : 'assets/images/image6.jpeg'
+
+	},
+	{
+		'id' : '7',
+		'category' : "Volly",
+		'description' : "Image 7",
+		'imageurl' : 'assets/images/image7.jpeg'
+
+	},
+	{
+		'id' : '8',
+		'category' : "Hockey",
+		'description' : "Image 8",
+		'imageurl' : 'assets/images/image8.jpeg'
+
+	},
+	{
+		'id' : '9',
+		'category' : "Hocky",
+		'description' : "Image 9",
+		'imageurl' : 'assets/images/image9.jpeg'
+
+	},
+	{
+		'id' : '10',
+		'category' : "Hockey",
+		'description' : "Image 10",
+		'imageurl' : 'assets/images/image10.jpeg'
+
+	},
+	{
+		'id' : '11',
+		'category' : "Hockey",
+		'description' : "Image 11",
+		'imageurl' : 'assets/images/image11.jpeg'
+
+	}
+];
